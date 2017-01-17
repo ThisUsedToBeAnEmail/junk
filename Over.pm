9cb9a606de40d82eba409ae865442a006506157e
LNATION <thisusedtobeanemail@gmail.com>
2017-01-17 19:07:26 +0100

    overload

---
+++ Over.pm
@@ -0,0 +1,43 @@
+package Over;
+
+my ($overload_this_number, $number ) = @ARGV;
+
+use overload
+    "+" => "plus",
+    "-" => "minus";
+
+sub new {
+    my $class = shift;
+    my $number = shift;
+    return bless \$number, $class;
+}
+
+sub minus {
+    my ($self, $other) = @_;
+    
+    my $result = $$self + $other;
+    return bless \$result, 'Over';
+}
+
+sub plus {
+    my ($self, $other) = @_;
+
+    my $result = $$self - $other;
+    return bless \$result, 'Over';
+}
+
+package main;
+
+use feature qw/say/;
+
+my $overloaded_number = Over->new($overload_this_number);
+
+$overloaded_number = $overloaded_number - $number;
+
+say $$overloaded_number;
+
+$overloaded_number = $overloaded_number + $number;
+
+say $$overloaded_number;
+
+1;
